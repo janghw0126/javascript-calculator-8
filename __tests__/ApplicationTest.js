7cad42a4661cc1f472ff6d2b631b5742c4f35d39
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-10-20 16:30:31 +0900

    test(App): 쉼표, 구분자 사용한 경우 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -40,6 +40,14 @@ describe("문자열 계산기", () => {
     expect(MissionUtils.Console.print).toHaveBeenCalledWith(expect.stringContaining("결과 : 5"));
   });
 
+  // 쉼표, 구분자 사용한 경우 테스트 추가
+  test("쉼표(,) 구분자 사용", async () => {
+    mockQuestions(["1,2,3"]);
+    const app = new App();
+    await app.run();
+    expect(MissionUtils.Console.print).toHaveBeenCalledWith(expect.stringContaining("결과 : 6"));
+  });
+
   test("커스텀 구분자 사용", async () => {
     const inputs = ["//;\\n1"];
     mockQuestions(inputs);
