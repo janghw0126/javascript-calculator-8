651b55706007d46993d6cdc41c69506d0467a3a7
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-10-20 14:29:48 +0900

    feat(validation): 커스텀 구분자 및 입력값 예외 처리 로직 추가

--- src/App.js
+++ src/App.js
@@ -20,6 +20,49 @@ class App {
       if (input === "") {
         return 0;
       }
+      // 숫자 담을 배열
+      let real_numbers = [];
+
+      // 1-2. 빈 문자열이 아닌 경우
+      // (1) 커스텀 구분자로 지정한 경우
+      if (input.slice(0, 2) === "//") {
+        // 커스텀 구분자를 기준으로 나누기
+        const parts = input.split(/\\n|\n/);
+        // 커스텀 구분자를 잘못 사용한 경우
+        if (parts.length < 2) {
+          throw new Error("[ERROR] 잘못된 입력 형식입니다.");
+        }
+
+        // 구분자 선언
+        const delimeter = parts[0].slice(2);
+
+        // 구분자가 두 개 이상인 경우
+        if (delimeter.length > 1) {
+          throw new Error("[ERROR] 구분자가 2개 이상 지정되었습니다.");
+        }
+
+        // 입력받는 값들 선언
+        const numbers = parts[1].split(delimeter);
+
+        // 숫자가 아닌 문자를 입력받을 경우
+        for (let i = 0; i < numbers.length; i++) {
+          if (numbers[i] === "") {
+            throw new Error("[ERROR] 빈 값이 있습니다.");
+          }
+          if (isNaN(numbers[i])) {
+            throw new Error("[ERROR] 입력받은 값은 숫자가 아닌 문자입니다.");
+          }
+          if (Number(numbers[i]) < 0) {
+            throw new Error("[ERROR] 입력받은 값은 음수입니다.");
+          }
+        }
+
+        // 숫자로 바꾸기
+        for (let i = 0; i < numbers.length; i++) {
+          real_numbers.push(Number(numbers[i]));
+        }
+      }
+
       
     }
 }
