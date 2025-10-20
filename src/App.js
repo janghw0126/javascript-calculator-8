0edae173afa9dbd9da53bba6bbc5db4b4e27d6f6
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-10-20 14:02:15 +0900

    feat(io): 문자열 입력 및 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 예외인 경우를 대비해서 try-catch 구조 사용
+    try{
+      // 문자열 입력받기
+      const input = await Console.readLineAsync("덧셈할 문자열을 입력해주세요.");
+      // 결과 result 변수 선언
+      const result = this.calculate(input.trim());
+      // 결과 출력
+      Console.print(`결과 : ${result}`);} catch(error){
+            Console.print(error.message);
+            throw error;
+          }
+        }
 }
 
 export default App;
